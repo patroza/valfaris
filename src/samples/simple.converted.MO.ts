e60e8ffafd866756d2c3757d7b01d0eedd6b0605
Patrick Roza <contact@patrickroza.com>
2020-05-21 19:57:47 +0200

    Update sample.

--- src/samples/simple.converted.MO.ts
+++ src/samples/simple.converted.MO.ts
@@ -1,20 +1,39 @@
-import { summonFor } from "@morphic-ts/batteries/lib/summoner-ESBAST"
+import * as MO from "./morphic"
 
-const { summon, tagged } = summonFor({})
+const SomeType_ = MO.summon((F) =>
+  F.interface(
+    {
+      h: F.date(),
+    },
+    "SomeType"
+  )
+)
+export interface SomeType extends MO.AType<typeof SomeType_> {}
+export interface SomeTypeRaw extends MO.EType<typeof SomeType_> {}
+export const SomeType = MO.AsOpaque<SomeTypeRaw, SomeType>()(SomeType_)
 
-const SomeInterface = summon((F) =>
+const NamedInterface_ = MO.summon((F) =>
+  F.interface(
+    {
+      title: F.string(),
+      arr: F.array(F.string()),
+    },
+    "NamedInterface"
+  )
+)
+export interface NamedInterface extends MO.AType<typeof NamedInterface_> {}
+export interface NamedInterfaceRaw extends MO.EType<typeof NamedInterface_> {}
+export const NamedInterface = MO.AsOpaque<NamedInterfaceRaw, NamedInterface>()(
+  NamedInterface_
+)
+
+const SomeInterface_ = MO.summon((F) =>
   F.interface(
     {
       a: F.array(F.string()),
       b: F.string(),
       c: F.number(),
-      d: F.interface(
-        {
-          value: F.string(),
-          key: F.number(),
-        },
-        "d"
-      ),
+      d: F.interface({ value: F.string(), key: F.number() }, "d"),
       e: NamedInterface(F),
       f: SomeType(F),
       g: F.array(SomeType(F)),
@@ -24,22 +43,8 @@ const SomeInterface = summon((F) =>
     "SomeInterface"
   )
 )
-
-const NamedInterface = summon((F) =>
-  F.interface(
-    {
-      title: F.string(),
-      arr: F.array(F.string()),
-    },
-    "NamedInterface"
-  )
-)
-
-const SomeType = summon((F) =>
-  F.interface(
-    {
-      h: F.date(),
-    },
-    "SomeType"
-  )
+export interface SomeInterface extends MO.AType<typeof SomeInterface_> {}
+export interface SomeInterfaceRaw extends MO.EType<typeof SomeInterface_> {}
+export const SomeInterface = MO.AsOpaque<SomeInterfaceRaw, SomeInterface>()(
+  SomeInterface_
 )
